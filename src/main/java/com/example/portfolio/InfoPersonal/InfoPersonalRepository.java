aaf6ac60f0107c93df2d31f7079e3f1b80f2ce35
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-03-25 15:48:14 -0300

    postman experiencia funcionando

--- src/main/java/com/example/portfolio/InfoPersonal/InfoPersonalRepository.java
+++ src/main/java/com/example/portfolio/InfoPersonal/InfoPersonalRepository.java
@@ -1,6 +1,5 @@
 package com.example.portfolio.InfoPersonal;
 
-import com.example.portfolio.InfoPersonal.infoPersonal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
